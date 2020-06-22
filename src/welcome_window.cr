9eb53c111fba5e5cad543abe0f97765344f791a7
Hugo Parente Lima <hugo.pl@gmail.com>
2020-06-22 02:30:14 -0300

    Scan user projects in another thread.

--- src/welcome_window.cr
+++ src/welcome_window.cr
@@ -3,26 +3,37 @@ require "./config"
 
 class WelcomeWindow < Window
   @projects_model : Gtk::ListStore
+  @tree_view : Gtk::TreeView
+  @rescan_btn : Gtk::Button
+  @scanning_projects = false
 
   def initialize(application : Application)
     builder = builder_for("welcome_window")
     super(application, builder)
 
     # Signals
-    tree_view = Gtk::TreeView.cast(builder["tree_view"])
-    tree_view.on_row_activated(&->project_activated(Gtk::TreeView, Gtk::TreePath, Gtk::TreeViewColumn))
+    @tree_view = Gtk::TreeView.cast(builder["tree_view"])
+    @tree_view.on_row_activated(&->project_activated(Gtk::TreeView, Gtk::TreePath, Gtk::TreeViewColumn))
 
-    rescan_btn = Gtk::Button.cast(builder["rescan_btn"])
-    rescan_btn.on_clicked(&->scan_projects(Gtk::Button))
+    @rescan_btn = Gtk::Button.cast(builder["rescan_btn"])
+    @rescan_btn.on_clicked(&->scan_projects(Gtk::Button))
+
+    overlay = Gtk::Overlay.cast(builder["overlay"])
+    @spinner = Gtk::Spinner.cast(builder["spinner"])
+    overlay.add_overlay(@spinner)
+    @spinner.hide
 
     @projects_model = Gtk::ListStore.cast(builder["projects_model"])
-    fill_projects_model
+
+    if Config.instance.scan_projects?
+      scan_projects
+    else
+      fill_projects_model
+    end
   end
 
   private def fill_projects_model
     config = Config.instance
-    scan_projects if config.scan_projects?
-
     home = Path.home.to_s
     config.projects.each do |project|
       project_dir = project.path.sub(home, "~")
@@ -31,19 +42,40 @@ class WelcomeWindow < Window
   end
 
   private def scan_projects(_button = nil)
-    config = Config.instance
+    return if @scanning_projects
 
-    t = Time.measure do
-      Project.scan_projects(Path.home).each do |project|
-        config.add_project(project.to_s)
+    @scanning_projects = true
+    @rescan_btn.sensitive = false
+    @tree_view.sensitive = false
+    @spinner.show
+    spawn do
+      config = Config.instance
+
+      t = Time.measure do
+        Project.scan_projects(Path.home).each do |project|
+          config.add_project(project.to_s)
+        end
       end
-    end
-    Log.info { "Scaning all projects in #{t}." }
-    config.scan_projects = false
-    config.filter_projects!
+      Log.info { "Scaning all projects in #{t}." }
+      config.scan_projects = false
+      config.filter_projects!
 
+      # Execute this in main thread
+      GLib.timeout(0, &->scan_projects_finished)
+    end
+  end
+
+  private def scan_projects_finished : Bool
     @projects_model.clear
     fill_projects_model
+    @rescan_btn.sensitive = true
+    @tree_view.sensitive = true
+    @scanning_projects = false
+    @spinner.hide
+    @tree_view.grab_focus
+
+    Log.info { "FIXME: Remove this message once https://github.com/jhass/crystal-gobject/issues/70 gets fixed." }
+    false
   end
 
   private def project_activated(view, path, _column)
