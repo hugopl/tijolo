hugopl/tijolo

commit 540601f4387371681cd69d935b882dfe13ebbfa1

Fork tijolo process to detach from terminal by default.
This is very useful to open projects in Tijolo while under a terminal.

There's a --no-fork CLI flag to avoid fork.

src/main.cr
@@ -12,17 +12,23 @@ require "./tijolo_log_backend"
 
 def setup_logger(options)
   logfile = options[:logfile]
-  logfile = File.join(Dir.tempdir, "tijolo.#{Process.pid}.log") if logfile.nil? && !STDOUT.tty?
-  backend = logfile ? Log::IOBackend.new(File.open(logfile, "w")) : Log::IOBackend.new
-
+  backend = if logfile || options[:fork]
+              logfile ||= File.join(Dir.tempdir, "tijolo.#{Process.pid}.log")
+              Log::IOBackend.new(File.open(logfile, "w"))
+            elsif STDOUT.tty?
+              Log::IOBackend.new
+            end
   Log.setup do |config|
     level = options[:log_level]
-    config.bind("*", level, backend)
+    config.bind("*", level, backend) if backend
     config.bind("*", level, TijoloLogBackend.instance)
   end
 end
 
 options = parse_args(ARGV)
+if options[:fork]
+  exit if Crystal::System::Process.fork
+end
 setup_logger(options)
 
 # Yes, always leak memory...

src/helper.cr
@@ -35,6 +35,7 @@ def parse_args(argv)
   gc_disabled = true # Leak all the things! See main.cr
   logfile = nil
   log_level = Config.instance.log_level
+  fork = true
 
   OptionParser.parse(argv) do |parser|
     parser.banner = "Usage: tijolo [OPTIONS] [FILE|DIRECTORY]..."
@@ -42,18 +43,19 @@ def parse_args(argv)
       puts "Tijolo version #{VERSION}"
       exit
     end
-    parser.on("--licenses", "Show license from Tijolo and their Crystal dependencies.") { }
-    parser.on("-h", "--help", "Show this help.") do
-      puts parser
-      exit
-    end
-    parser.on("--logfile=FILE", "Where to save log, default to STDOUT or /tmp/tijolo.PID.log if not on a tty.") do |file|
-      logfile = file
-    end
     parser.on("--debug", "Enable some debug stuff, like log all LSP communication.") { log_level = Log::Severity::Debug }
     parser.on("--enable-gc", "Enable garbage collector (see https://github.com/jhass/crystal-gobject/issues/69).") do
       gc_disabled = false
     end
+    parser.on("-h", "--help", "Show this help.") do
+      puts parser
+      exit
+    end
+    parser.on("--licenses", "Show license from Tijolo and their Crystal dependencies.") { }
+    parser.on("--logfile=FILE", "Where to save log, default to STDOUT or /tmp/tijolo.PID.log if not on a tty.") do |file|
+      logfile = file
+    end
+    parser.on("--no-fork", "Do not fork process.") { fork = false }
     parser.invalid_option do |flag|
       STDERR.puts "ERROR: #{flag} is not a valid option."
       STDERR.puts parser
@@ -64,5 +66,6 @@ def parse_args(argv)
   {locations:   argv,
    gc_disabled: gc_disabled,
    logfile:     logfile,
-   log_level:   log_level}
+   log_level:   log_level,
+   fork:        fork}
 end

tijolo.desktop
@@ -1,6 +1,6 @@
 [Desktop Entry]
 Type=Application
-Exec=tijolo %F
+Exec=tijolo --no-fork %F
 Name=Tijolo
 GenericName=Lightweight, keyboard-oriented IDE for the masses
 Terminal=false
@@ -12,4 +12,4 @@ Actions=new-window;
 
 [Desktop Action new-window]
 Name=New Window
-Exec=tijolo
+Exec=tijolo --no-fork

commit ef96ca2da268a22303ea6c17204eace04d46e5ea

Log to `/tmp/tijolo.PID.log` when not running on a tty and user didn't pass --logfile.
Regression introduced by 540601f4387371681cd69d935b882dfe13ebbfa1.

src/main.cr
@@ -12,10 +12,11 @@ require "./tijolo_log_backend"
 
 def setup_logger(options)
   logfile = options[:logfile]
-  backend = if logfile
+  is_tty = STDOUT.tty?
+  backend = if logfile || !is_tty
               logfile ||= File.join(Dir.tempdir, "tijolo.#{Process.pid}.log")
               Log::IOBackend.new(File.open(logfile, "w"))
-            elsif STDOUT.tty?
+            elsif is_tty
               Log::IOBackend.new
             end
   Log.setup do |config|