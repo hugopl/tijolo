3b0b4bbc5e0e17a5d77d9649e75f5b25c88b894a
Hugo Parente Lima <hugo.pl@gmail.com>
2020-12-09 16:14:28 -0300

    Simplify LSP server capabilities struct.

--- src/lsp/server_capabilities.cr
+++ src/lsp/server_capabilities.cr
@@ -1,60 +1,48 @@
 module LSP
-  enum TextDocumentSyncKind
-    None
-    Full
-    Incremental
+  enum TextDocumentSyncKind : Int32
+    None        = 0
+    Full        = 1
+    Incremental = 2
   end
 
   struct SaveOptions
     include JSON::Serializable
 
     @[JSON::Field(key: "includeText")]
-    getter? include_text : Bool?
+    getter? include_text : Bool = false
   end
 
   struct TextDocumentSyncOptions
     include JSON::Serializable
 
     @[JSON::Field(key: "openClose")]
-    getter? open_close : Bool?
-    getter change : TextDocumentSyncKind?
+    getter? open_close : Bool = false
+    getter change = LSP::TextDocumentSyncKind::None
     @[JSON::Field(key: "willSave")]
-    getter will_save : Bool?
+    getter? will_save : Bool = false
     @[JSON::Field(key: "willSaveWaitUntil")]
-    getter will_save_wait_until : Bool?
-    getter save : SaveOptions | Bool | Nil
-
-    def full?
-      @change ? @change.not_nil!.full? : false
-    end
-
-    def incremental?
-      @change ? @change.not_nil!.incremental? : false
-    end
-
-    def none?
-      @change ? @change.not_nil!.none? : true
-    end
+    getter will_save_wait_until : Bool = false
+    getter save : SaveOptions | Bool = false
   end
 
   struct CompletionOptions
     include JSON::Serializable
 
     @[JSON::Field(key: "resolveProvider")]
-    @resolve_provider : Bool? = false
+    @resolve_provider : Bool = false
     @[JSON::Field(key: "triggerCharacters")]
-    @trigger_characters : Array(String)?
+    @trigger_characters = [] of String
     @[JSON::Field(key: "allCommitCharacters")]
-    @all_commit_characters : Array(String)?
+    @all_commit_characters = [] of String
   end
 
   struct SignatureHelpOptions
     include JSON::Serializable
 
     @[JSON::Field(key: "triggerCharacters")]
-    @trigger_characters : Array(String)?
+    @trigger_characters = [] of String
     @[JSON::Field(key: "retriggerCharacters")]
-    @retrigger_characters : Array(String)?
+    @retrigger_characters = [] of String
   end
 
   struct CodeActionOptions
@@ -68,7 +56,7 @@ module LSP
     include JSON::Serializable
 
     @[JSON::Field(key: "resolveProvider")]
-    @resolve_provider : Bool?
+    @resolve_provider : Bool = false
   end
 
   struct DocumentOnTypeFormattingOptions
@@ -85,7 +73,7 @@ module LSP
     include JSON::Serializable
 
     @[JSON::Field(key: "resolveProvider")]
-    @resolve_provider : Bool? = false
+    @resolve_provider : Bool = false
   end
 
   struct ExecuteCommandOptions
@@ -98,7 +86,7 @@ module LSP
     include JSON::Serializable
 
     @[JSON::Field(key: "prepareProvider")]
-    @prepare_provider : Bool? = false
+    @prepare_provider : Bool = false
   end
 
   struct WorkspaceFoldersServerCapabilities
@@ -106,7 +94,7 @@ module LSP
 
     @supported : Bool?
     @[JSON::Field(key: "changeNotifications")]
-    @change_notifications : Bool | String | Nil
+    @change_notifications : Bool | String = false
   end
 
   struct WorkspaceOptions
@@ -119,11 +107,11 @@ module LSP
   # FIXME: This struct is REALLY imcomplete... and will fail for a lot of servers, I'm fixing when I see a server using a
   #        sub struct that is not here, to fix all is very easy, but boring and time consuming... just read the LSP spec
   #        and create the structs.
-  struct ServerCapabilities
+  class ServerCapabilities
     include JSON::Serializable
 
     @[JSON::Field(key: "codeActionProvider")]
-    @code_action_provider : CompletionOptions | Bool | Nil = false
+    @code_action_provider : CompletionOptions | Bool = false
 
     @[JSON::Field(key: "codeLensProvider")]
     @code_lens_provider : CodeLensOptions?
@@ -132,16 +120,16 @@ module LSP
     @completion_provider : CompletionOptions?
 
     @[JSON::Field(key: "declarationProvider")]
-    @declaration_provider : Bool? = false
+    @declaration_provider : Bool = false
 
     @[JSON::Field(key: "definitionProvider")]
-    @definition_provider : Bool? = false
+    getter? definition_provider : Bool = false
 
     @[JSON::Field(key: "documentFormattingProvider")]
-    @document_formatting_provider : Bool? = false
+    @document_formatting_provider : Bool = false
 
     @[JSON::Field(key: "documentHighlightProvider")]
-    @document_highlight_provider : Bool? = false
+    @document_highlight_provider : Bool = false
 
     @[JSON::Field(key: "documentLinkProvider")]
     @document_link_provider : DocumentLinkOptions?
@@ -150,42 +138,42 @@ module LSP
     @document_on_type_formatting_provider : DocumentOnTypeFormattingOptions?
 
     @[JSON::Field(key: "documentRangeFormattingProvider")]
-    @document_range_formatting_provider : Bool? = false
+    @document_range_formatting_provider : Bool = false
 
     @[JSON::Field(key: "documentSymbolProvider")]
-    @document_symbol_provider : Bool? = false
+    getter? document_symbol_provider : Bool = false
 
     @[JSON::Field(key: "executeCommandProvider")]
     @execute_command_provider : ExecuteCommandOptions?
 
     @[JSON::Field(key: "foldingRangeProvider")]
-    @folding_range_provider : Bool? = false
+    @folding_range_provider : Bool = false
 
     @[JSON::Field(key: "hoverProvider")]
-    @hover_provider : Bool? = false
+    @hover_provider : Bool = false
 
     @[JSON::Field(key: "implementationProvider")]
-    @implementation_provider : Bool? = false
+    @implementation_provider : Bool = false
 
     @[JSON::Field(key: "referencesProvider")]
-    @references_provider : Bool? = false
+    @references_provider : Bool = false
 
     @[JSON::Field(key: "renameProvider")]
-    @rename_provider : RenameOptions | Bool | Nil = false
+    @rename_provider : RenameOptions | Bool = false
 
     @[JSON::Field(key: "signatureHelpProvider")]
     @signature_help_provider : SignatureHelpOptions?
 
     @[JSON::Field(key: "textDocumentSync")]
-    @text_document_sync : TextDocumentSyncOptions | Int32 | Nil
+    @text_document_sync : TextDocumentSyncOptions | Int32 = LSP::TextDocumentSyncKind::None.value
 
     @[JSON::Field(key: "typeDefinitionProvider")]
-    @type_definition_provider : Bool? = false
+    @type_definition_provider : Bool = false
 
     @workspace : WorkspaceOptions?
 
     @[JSON::Field(key: "workspaceSymbolProvider")]
-    @workspace_symbol_provider : Bool? = false
+    @workspace_symbol_provider : Bool = false
 
     def initialize
     end
@@ -198,18 +186,9 @@ module LSP
     def text_document_sync : TextDocumentSyncKind
       sync = @text_document_sync
       case sync
-      in Int32                   then TextDocumentSyncKind.new(sync)
-      in Nil                     then TextDocumentSyncKind::None
-      in TextDocumentSyncOptions then sync.change || TextDocumentSyncKind::None
+      in Int32                   then TextDocumentSyncKind.from_value(sync)
+      in TextDocumentSyncOptions then sync.change
       end
-    end
-
-    def definition_provider? : Bool
-      !!@definition_provider
-    end
-
-    def document_symbol_provider? : Bool
-      !!@document_symbol_provider
     end
   end
 end
