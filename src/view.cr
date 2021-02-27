hugopl/tijolo

commit 03f2be5a21667fca1170b8e24f15f4f02c9fa98a

Better handle file renames and modifications by external programs.
This include monitor changes of open non-project files.

src/view.cr
@@ -41,6 +41,11 @@ abstract class View
   delegate show_all, to: @view_widget
   delegate grab_focus, to: @view_widget
 
+  @monitor : Gio::FileMonitor?
+
+  # True during a saving operation
+  private getter? saving = false
+
   def initialize(@view_widget : Gtk::Widget, file_path : Path? = nil, @project_path = nil)
     builder = builder_for("view")
     @widget = Gtk::Widget.cast(builder["root"])
@@ -75,6 +80,10 @@ abstract class View
     Gtk::ModelButton.cast(builder["copy_relative_path"]).action_target_value = variant_self
   end
 
+  def finalize
+    @monitor.try(&.cancel)
+  end
+
   def self.reset_untitled_count
     @@untitled_count = -1
   end
@@ -84,11 +93,41 @@ abstract class View
   end
 
   def file_path=(file_path : Path) : Nil
+    return if file_path == @file_path
+
     @file_path = file_path
     self.readonly = !File.writable?(file_path) if File.exists?(file_path)
     @virtual = false
     self.label = relative_path_label(file_path, @project_path)
     notify_view_file_path_changed(self)
+    setup_monitor
+  end
+
+  private def setup_monitor
+    monitor = @monitor
+    file_path = @file_path
+    return if monitor || virtual? || file_path.nil?
+
+    @monitor = monitor = Gio::File.new_for_path(file_path.to_s).monitor_file(:none, nil)
+    monitor.on_changed(&->file_changed_event(Gio::FileMonitor, Gio::File, Gio::File?, Gio::FileMonitorEvent))
+  end
+
+  private def file_changed_event(_monitor : Gio::FileMonitor, file : Gio::File, other_file : Gio::File?, event : Gio::FileMonitorEvent)
+    return if saving?
+
+    case event
+    when .changes_done_hint? # At least on Linux I never see the RENAME event.
+      file_path = Path.new(file.parse_name)
+      self.file_path = file_path
+    when .changed?
+      if can_reload?
+        reload
+      else
+        was_modified_before = externally_modified?
+        externally_modified!
+        update_header unless was_modified_before
+      end
+    end
   end
 
   def label=(@label : String)
@@ -127,14 +166,14 @@ abstract class View
 
   def header_text : String
     # TODO: Find a better/nicer way to inform view status on UI
-    modified = if modified?
-                 "✱"
-               elsif readonly?
-                 "<readonly>"
-               end
-    maximized = "<maximized>" if @maximized
-    separator = " " if maximized || modified
-    "#{label}#{separator}#{modified}#{maximized}"
+    String.build do |str|
+      str << label
+      str << " " if readonly? || maximized? || externally_modified? || modified?
+      str << "*" if modified?
+      str << "<readonly>" if readonly?
+      str << "<maximized>" if maximized?
+      str << "<externally modified>" if externally_modified?
+    end
   end
 
   def modified? : Bool
@@ -171,6 +210,7 @@ abstract class View
   end
 
   def can_reload? : Bool
+    return true if readonly?
     return false if modified?
 
     # A more accurate approach would check if the view contents matches the contents on disk, if not, return false. However
@@ -184,9 +224,20 @@ abstract class View
   end
 
   def save : Nil
+    return if readonly?
+
+    file_path = @file_path
+    raise AppError.new("Attempt to save a file without a name.") if file_path.nil?
+
     @last_saved_at = Time.monotonic
     @externally_modified = false
-    self.readonly = false
+    @saving = true
+    do_save
+  ensure
+    @saving = false
+  end
+
+  protected def do_save : Nil
   end
 
   def to_s(io : IO) : IO

src/ide_window.cr
@@ -114,19 +114,6 @@ class IdeWindow < Window
     ask_about_externally_modified_files
   end
 
-  def project_file_content_changed(path : Path)
-    view = @open_files.view(path)
-    return if view.nil? || view.externally_modified?
-
-    if view.readonly?
-      view.reload
-    else
-      view.externally_modified!
-      # TODO: Show a passive banner and let user press F5 to reload or ESC to ignore instead of an annoying dialog.
-      ask_about_externally_modified_files
-    end
-  end
-
   def project_load_finished
     @sidebar.show_all
     # Setup title bar

src/project.cr
@@ -18,9 +18,6 @@ module ProjectListener
   def project_files_changed
   end
 
-  def project_file_content_changed(path : Path)
-  end
-
   def project_load_finished
   end
 end

src/project_monitor.cr
@@ -1,10 +1,12 @@
 require "log"
 require "./project"
 
+# Each directory under project root has a directory monitor.
+# Each open file has its own file monitor, handled by the View class itself.
 class ProjectMonitor
   include ProjectListener
 
-  # The is a directory path relative to the project root
+  # The string key is a directory path relative to the project root
   @monitors = Hash(String, Gio::FileMonitor).new
 
   def initialize(@project : Project)
@@ -41,8 +43,6 @@ class ProjectMonitor
       @project.remove_path(file_path)
     when .renamed?
       @project.rename_path(file_path, other_path) if other_path
-    when .changed?
-      @project.notify_project_file_content_changed(file_path)
     end
   end
 

src/text_view.cr
@@ -87,15 +87,9 @@ class TextView < View
     true
   end
 
-  def save
-    return if readonly?
-
-    file_path = @file_path
-    raise AppError.new("Attempt to save a file without a name.") if file_path.nil?
-
+  def do_save
     remove_all_trailing_spaces! if @trim_trailing_white_space_on_save
-    super
-    File.write(file_path, text)
+    File.write(@file_path.not_nil!, text)
     @buffer.modified = false
 
     language.notify_did_save(self)

commit bf5924fac7699f6e46205655e5216d307dea3e2f

Fix file reload on save issue introduced by 03f2be5a21667fca1170b8e24f15f4f02c9fa98a.

src/view.cr
@@ -113,13 +113,10 @@ abstract class View
   end
 
   private def file_changed_event(_monitor : Gio::FileMonitor, file : Gio::File, other_file : Gio::File?, event : Gio::FileMonitorEvent)
-    return if saving?
-
-    case event
-    when .changes_done_hint? # At least on Linux I never see the RENAME event.
-      file_path = Path.new(file.parse_name)
-      self.file_path = file_path
-    when .changed?
+    if event.changes_done_hint? # At least on Linux I never see the RENAME event.
+      @saving = false
+      self.file_path = Path.new(file.parse_name)
+    elsif event.changed? && !saving?
       if can_reload?
         reload
       else
@@ -233,8 +230,9 @@ abstract class View
     @externally_modified = false
     @saving = true
     do_save
-  ensure
+  rescue e
     @saving = false
+    raise e
   end
 
   protected def do_save : Nil