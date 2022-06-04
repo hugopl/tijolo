498e5c45af5232a2eb5fca8ebfc8aeac08097861
Hugo Parente Lima <hugo.pl@gmail.com>
2022-06-03 22:50:37 -0300

    Use GResource to load data files.

--- src/main.cr
+++ src/main.cr
@@ -5,5 +5,6 @@ require "./tijolo_application"
 
 {% raise "This branch isn't ready yet, try the latest tag (v0.7.1) instead." unless flag?(:i_know_what_im_doing) %}
 
+Gio.register_resource("tijolo_resources.xml")
 app = TijoloApplication.new
 exit(app.run)
