e75712feac416ac8d70759f7bdc0ff554f054a7a
Hugo Parente Lima <hugo.pl@gmail.com>
2022-06-17 19:56:32 -0300

    Move resource files to `data/` and remove some aliases.

--- src/main.cr
+++ src/main.cr
@@ -5,6 +5,6 @@ require "./tijolo_application"
 
 {% raise "This branch isn't ready yet, try the latest tag (v0.7.1) instead." unless flag?(:i_know_what_im_doing) %}
 
-Gio.register_resource("tijolo_resources.xml")
+Gio.register_resource("data/resources.xml", source_dir: "data")
 app = TijoloApplication.new
 exit(app.run)
