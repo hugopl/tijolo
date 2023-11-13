4f76e5db2c95e94263a217c9f8cfdefeb66d0880
Hugo Parente Lima <hugo.pl@gmail.com>
2023-11-13 10:14:32 -0300

    Detect file mimetype based on contents if can't guess the by extension.

--- src/gsv/code_language.cr
+++ src/gsv/code_language.cr
@@ -14,9 +14,21 @@ struct CodeLanguage
     line_comment.to_slice
   end
 
-  def self.detect(resource : Path) : CodeLanguage
-    lang = GtkSource::LanguageManager.default.guess_language(resource.to_s, nil)
+  def self.detect(file : Path) : CodeLanguage
+    lang_manager = GtkSource::LanguageManager.default
+    lang = lang_manager.guess_language(file.to_s, nil)
+    if lang.nil?
+      type = content_type(file)
+      lang = lang_manager.guess_language(nil, type) if type
+    end
     lang_id = lang ? lang.id : ""
     CodeLanguage.new(lang_id, lang)
+  end
+
+  private def self.content_type(file : Path) : String?
+    data = File.read(file).to_slice
+    Gio.content_type_guess(file.basename, data)
+  rescue IO::Error
+    nil
   end
 end
