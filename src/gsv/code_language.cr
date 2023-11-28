954c9d0e63246f5fd5be9af66ce88b6eb336b99d
Hugo Parente Lima <hugo.pl@gmail.com>
2023-11-28 19:08:25 -0300

    Add ugly workaround to identify Dockerfile in GtkSourceView.

--- src/gsv/code_language.cr
+++ src/gsv/code_language.cr
@@ -22,18 +22,10 @@ struct CodeLanguage
   def self.detect(file : Path) : CodeLanguage
     lang_manager = GtkSource::LanguageManager.default
     lang = lang_manager.guess_language(file.to_s, nil)
-    if lang.nil?
-      type = content_type(file)
-      lang = lang_manager.guess_language(nil, type) if type
-    end
+    # FIXME: Remove this Ugly workaround
+    lang = lang_manager.language("docker") if lang.nil? && file.basename == "Dockerfile"
+
     lang_id = lang ? lang.id : ""
     CodeLanguage.new(lang_id, lang)
-  end
-
-  private def self.content_type(file : Path) : String?
-    data = File.read(file).to_slice
-    Gio.content_type_guess(file.basename, data)
-  rescue IO::Error
-    nil
   end
 end
