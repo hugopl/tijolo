f55a6516cc3af8da06ca6c71e87184cf39ce34a8
Hugo Parente Lima <hugo.pl@gmail.com>
2023-12-09 23:57:53 -0300

    Guess syntax highlight also by file contents.

--- src/code_language.cr
+++ src/code_language.cr
@@ -21,11 +21,20 @@ struct CodeLanguage
 
   def self.detect(file : Path) : CodeLanguage
     lang_manager = GtkSource::LanguageManager.default
-    lang = lang_manager.guess_language(file.to_s, nil)
-    # FIXME: Remove this Ugly workaround
-    lang = lang_manager.language("docker") if lang.nil? && file.basename == "Dockerfile"
+    lang = lang_manager.language("docker") if file.basename == "Dockerfile"
+    lang ||= lang_manager.guess_language(file.to_s, nil)
+    lang ||= detect_by_content(file)
 
     lang_id = lang ? lang.id : ""
     CodeLanguage.new(lang_id, lang)
+  end
+
+  def self.detect_by_content(file) : GtkSource::Language?
+    File.open(file) do |io|
+      contents = Bytes.new(2048) # No need to read the entire file
+      contents_size = io.read(contents)
+      mime = Gio.content_type_guess(file.to_s, contents[0, contents_size])
+      return GtkSource::LanguageManager.default.guess_language(file.to_s, mime)
+    end
   end
 end
