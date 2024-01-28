hugopl/tijolo

commit 9df6c08c344a164b543b1d93ecb208d0fd5c7a57

Implement new find/replace dialog.
A lot of 🐛 still.

src/document_view.cr
@@ -28,8 +28,6 @@ abstract class DocumentView < View
       menu_model: copy_path_menu_model)
     copy_path_btn.add_css_class("flat")
     header_center_box.prepend(copy_path_btn)
-
-    setup_actions
   end
 
   def save : Nil
@@ -120,7 +118,6 @@ abstract class DocumentView < View
       action_group.add_action(action)
       @resource_actions << action
     {% end %}
-    setup_actions(action_group)
 
     insert_action_group("view", action_group)
   end

data/icons/hicolor/scalable/actions/tijolo-case-sensitive-symbolic.svg
@@ -1,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   width="16mm"
+   height="16mm"
+   viewBox="0 0 16 16"
+   version="1.1"
+   id="svg1"
+   inkscape:version="1.3.2 (091e20ef0f, 2023-11-25, custom)"
+   sodipodi:docname="tijolo-case-sensitive-symbolic.svg"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:cc="http://creativecommons.org/ns#">
+  <sodipodi:namedview
+     id="namedview1"
+     pagecolor="#ffffff"
+     bordercolor="#000000"
+     borderopacity="0.25"
+     inkscape:showpageshadow="2"
+     inkscape:pageopacity="0.0"
+     inkscape:pagecheckerboard="0"
+     inkscape:deskcolor="#d1d1d1"
+     inkscape:document-units="mm"
+     inkscape:zoom="5.6301908"
+     inkscape:cx="22.02412"
+     inkscape:cy="28.062992"
+     inkscape:window-width="1920"
+     inkscape:window-height="1011"
+     inkscape:window-x="0"
+     inkscape:window-y="0"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="layer1" />
+  <defs
+     id="defs1" />
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <path
+       d="m 175.52614,246.72935 -0.3302,-1.0795 h -1.651 l -0.3302,1.0795 h -1.03505 l 1.6002,-4.55295 h 1.17475 l 1.60655,4.55295 z m -0.889,-2.94005 q -0.0317,-0.10795 -0.0825,-0.27305 -0.0508,-0.1651 -0.1016,-0.33655 -0.0508,-0.17145 -0.0825,-0.29845 -0.0317,0.127 -0.0889,0.3175 -0.0508,0.18415 -0.1016,0.3556 -0.0444,0.1651 -0.0698,0.23495 l -0.32385,1.0541 h 1.1811 z m 3.84175,-0.5969 q 0.6985,0 1.0668,0.3048 0.37465,0.29845 0.37465,0.92075 v 2.3114 h -0.6604 l -0.18415,-0.4699 h -0.0254 q -0.22225,0.2794 -0.4699,0.4064 -0.24765,0.127 -0.67945,0.127 -0.46355,0 -0.76835,-0.2667 -0.3048,-0.27305 -0.3048,-0.83185 0,-0.55245 0.38735,-0.8128 0.38735,-0.2667 1.16205,-0.2921 l 0.60325,-0.0191 v -0.1524 q 0,-0.27305 -0.14605,-0.40005 -0.1397,-0.127 -0.3937,-0.127 -0.254,0 -0.4953,0.0762 -0.2413,0.0699 -0.4826,0.1778 l -0.31115,-0.64135 q 0.2794,-0.14605 0.61595,-0.2286 0.3429,-0.0825 0.7112,-0.0825 z m 0.13335,1.9431 q -0.4572,0.0127 -0.635,0.1651 -0.1778,0.1524 -0.1778,0.40005 0,0.2159 0.127,0.31115 0.127,0.0889 0.3302,0.0889 0.3048,0 0.51435,-0.1778 0.20955,-0.18415 0.20955,-0.51435 v -0.28575 z"
+       id="text1"
+       style="font-weight:bold;font-size:6.35px;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans, Bold';white-space:pre;inline-size:133.397;fill:#2e3436;stroke-width:0.264583"
+       inkscape:highlight-color="#000000"
+       transform="matrix(2.0110619,0,0,2.0110619,-346.07452,-483.60175)"
+       aria-label="Aa" />
+  </g>
+  <metadata
+     id="metadata1">
+    <rdf:RDF>
+      <cc:License
+         rdf:about="http://artlibre.org/licence/lal">
+        <cc:permits
+           rdf:resource="http://creativecommons.org/ns#Reproduction" />
+        <cc:permits
+           rdf:resource="http://creativecommons.org/ns#Distribution" />
+        <cc:permits
+           rdf:resource="http://creativecommons.org/ns#DerivativeWorks" />
+        <cc:requires
+           rdf:resource="http://creativecommons.org/ns#ShareAlike" />
+        <cc:requires
+           rdf:resource="http://creativecommons.org/ns#Notice" />
+        <cc:requires
+           rdf:resource="http://creativecommons.org/ns#Attribution" />
+      </cc:License>
+      <cc:Work
+         rdf:about="">
+        <cc:license
+           rdf:resource="http://artlibre.org/licence/lal" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+</svg>

data/icons/hicolor/scalable/actions/tijolo-regex-symbolic.svg
@@ -1,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   width="16mm"
+   height="16mm"
+   viewBox="0 0 16 16"
+   version="1.1"
+   id="svg1"
+   inkscape:version="1.3.2 (091e20ef0f, 2023-11-25, custom)"
+   sodipodi:docname="tijolo-regex-symbolic.svg"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:cc="http://creativecommons.org/ns#">
+  <sodipodi:namedview
+     id="namedview1"
+     pagecolor="#ffffff"
+     bordercolor="#000000"
+     borderopacity="0.25"
+     inkscape:showpageshadow="2"
+     inkscape:pageopacity="0.0"
+     inkscape:pagecheckerboard="0"
+     inkscape:deskcolor="#d1d1d1"
+     inkscape:document-units="mm"
+     inkscape:zoom="5.6301908"
+     inkscape:cx="22.201734"
+     inkscape:cy="28.062992"
+     inkscape:window-width="1920"
+     inkscape:window-height="1011"
+     inkscape:window-x="0"
+     inkscape:window-y="0"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="layer1" />
+  <defs
+     id="defs1" />
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <path
+       d="m 172.54164,246.28485 q 0,-0.2921 0.15875,-0.4064 0.15875,-0.12065 0.38735,-0.12065 0.22225,0 0.381,0.12065 0.15875,0.1143 0.15875,0.4064 0,0.2794 -0.15875,0.4064 -0.15875,0.12065 -0.381,0.12065 -0.2286,0 -0.38735,-0.12065 -0.15875,-0.127 -0.15875,-0.4064 z m 3.5814,-4.3815 -0.127,1.143 1.1557,-0.32385 0.1016,0.78105 -1.0541,0.0762 0.69215,0.92075 -0.70485,0.37465 -0.4826,-0.97155 -0.42545,0.9652 -0.73025,-0.3683 0.6858,-0.92075 -1.04775,-0.0825 0.12065,-0.7747 1.1303,0.32385 -0.127,-1.143 z"
+       id="text1"
+       style="font-weight:bold;font-size:6.35px;font-family:'Noto Sans';-inkscape-font-specification:'Noto Sans Bold';white-space:pre;inline-size:133.397;fill:#2e3436;stroke-width:0.264583"
+       inkscape:highlight-color="#000000"
+       transform="matrix(3.1611648,0,0,3.1611648,-544.9354,-764.39899)"
+       aria-label=".*" />
+  </g>
+  <metadata
+     id="metadata1">
+    <rdf:RDF>
+      <cc:License
+         rdf:about="http://artlibre.org/licence/lal">
+        <cc:permits
+           rdf:resource="http://creativecommons.org/ns#Reproduction" />
+        <cc:permits
+           rdf:resource="http://creativecommons.org/ns#Distribution" />
+        <cc:permits
+           rdf:resource="http://creativecommons.org/ns#DerivativeWorks" />
+        <cc:requires
+           rdf:resource="http://creativecommons.org/ns#ShareAlike" />
+        <cc:requires
+           rdf:resource="http://creativecommons.org/ns#Notice" />
+        <cc:requires
+           rdf:resource="http://creativecommons.org/ns#Attribution" />
+      </cc:License>
+      <cc:Work
+         rdf:about="">
+        <cc:license
+           rdf:resource="http://artlibre.org/licence/lal" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+</svg>

data/resources.xml
@@ -3,5 +3,7 @@
   <gresource prefix="/io/github/hugopl/Tijolo">
     <file>style.css</file>
     <file preprocess="xml-stripblanks">gtk/help-overlay.ui</file>
+    <file preprocess="xml-stripblanks">icons/hicolor/scalable/actions/tijolo-regex-symbolic.svg</file>
+    <file preprocess="xml-stripblanks">icons/hicolor/scalable/actions/tijolo-case-sensitive-symbolic.svg</file>
   </gresource>
 </gresources>

data/style.css
@@ -15,7 +15,7 @@ view .view-header {
   background-color: @window_bg_color;
 }
 
-view:focus-within {
+view:focus-within .view-header {
   color: @accent_color;
 }
 

spec/find_replace_spec.cr
@@ -1,0 +1,80 @@
+require "spec"
+require "../src/find_replace_options"
+
+describe FindReplaceOptions do
+  context "can parse" do
+    it "/foo" do
+      opts = FindReplaceOptions.parse("/foo").not_nil!
+      opts.find_term.should eq("foo")
+      opts.replace_term.should eq(nil)
+      opts.case_sensitive?.should eq(true)
+      opts.regex?.should eq(true)
+    end
+
+    it "/foo/" do
+      opts = FindReplaceOptions.parse("/foo/").not_nil!
+      opts.find_term.should eq("foo")
+      opts.replace_term.should eq("")
+      opts.case_sensitive?.should eq(true)
+      opts.regex?.should eq(true)
+    end
+
+    it "/foo/bar/i" do
+      opts = FindReplaceOptions.parse("/foo/bar/i").not_nil!
+      opts.find_term.should eq("foo")
+      opts.replace_term.should eq("bar")
+      opts.case_sensitive?.should eq(false)
+      opts.regex?.should eq(true)
+    end
+
+    it "/foo\\/bar" do
+      opts = FindReplaceOptions.parse("/foo\\/bar").not_nil!
+      opts.find_term.should eq("foo/bar")
+      opts.replace_term.should eq(nil)
+      opts.case_sensitive?.should eq(true)
+      opts.regex?.should eq(true)
+    end
+
+    it "foo" do
+      opts = FindReplaceOptions.parse("foo").not_nil!
+      opts.find_term.should eq("foo")
+      opts.regex?.should eq(false)
+      opts.case_sensitive?.should eq(false)
+    end
+
+    it "foo\\n" do
+      opts = FindReplaceOptions.parse("foo\\n").not_nil!
+      opts.find_term.should eq("foo\n")
+      opts.regex?.should eq(false)
+      opts.case_sensitive?.should eq(false)
+    end
+
+    it "foo\\t" do
+      opts = FindReplaceOptions.parse("foo\\t").not_nil!
+      opts.find_term.should eq("foo\t")
+      opts.regex?.should eq(false)
+      opts.case_sensitive?.should eq(false)
+    end
+
+    it "empty string" do
+      opts = FindReplaceOptions.parse("").not_nil!
+      opts.find_term.should eq("")
+      opts.regex?.should eq(false)
+      opts.case_sensitive?.should eq(false)
+    end
+
+    it "/" do
+      opts = FindReplaceOptions.parse("/").not_nil!
+      opts.find_term.should eq("")
+      opts.regex?.should eq(true)
+      opts.case_sensitive?.should eq(true)
+    end
+
+    it "/\\w*" do
+      opts = FindReplaceOptions.parse("/\\w*").not_nil!
+      opts.find_term.should eq("\\w*")
+      opts.regex?.should eq(true)
+      opts.case_sensitive?.should eq(true)
+    end
+  end
+end

src/application_window.cr
@@ -178,6 +178,9 @@ class ApplicationWindow < Adw::ApplicationWindow
                move_viewport_page_up:   ->move_viewport_page_up,
                move_viewport_page_down: ->move_viewport_page_down,
                fullscreen:              ->toggle_fullscreen,
+               find:                    ->find,
+               find_next:               ->find_next,
+               find_prev:               ->find_prev,
     }
     actions.each do |name, closure|
       action = Gio::SimpleAction.new(name.to_s, nil)
@@ -409,8 +412,12 @@ class ApplicationWindow < Adw::ApplicationWindow
     with_current_view(&.paste_from_clipboard)
   end
 
-  {% for action in %w(sort_lines comment_code move_lines_up move_lines_down move_viewport_line_up move_viewport_line_down
-                     move_viewport_page_up move_viewport_page_down) %}
+  {% for action in %w(sort_lines
+                     comment_code
+                     move_lines_up move_lines_down
+                     move_viewport_line_up move_viewport_line_down
+                     move_viewport_page_up move_viewport_page_down
+                     find find_next find_prev) %}
   private def {{ action.id }}
     with_current_view do |view|
       view.{{ action.id }} if view.responds_to?({{ action.id.symbolize }})

src/code_editor.cr
@@ -7,12 +7,6 @@ require "./init_source_view"
 class CodeEditor < GtkSource::View
   include CodeCommenter
 
-  @search_context : GtkSource::SearchContext
-  @search_settings : GtkSource::SearchSettings
-  @search_mark : Gtk::TextMark
-
-  @[GObject::Property]
-  property search_occurences : String = ""
   getter language : CodeLanguage
 
   def initialize(source : IO?, @language : CodeLanguage)
@@ -23,11 +17,6 @@ class CodeEditor < GtkSource::View
       background_pattern: :grid,
       smart_home_end: :before,
       buffer: CodeBuffer.new)
-    @search_settings = GtkSource::SearchSettings.new(wrap_around: true)
-    @search_context = GtkSource::SearchContext.new(buffer, @search_settings)
-    @search_mark = buffer.insert
-    @search_context.notify_signal["occurrences-count"].connect { update_label_occurrences }
-
     key = Gtk::EventControllerKey.new(propagation_phase: :target)
     key.key_pressed_signal.connect(->on_key_press(UInt32, UInt32, Gdk::ModifierType))
     add_controller(key)
@@ -102,7 +91,7 @@ class CodeEditor < GtkSource::View
   {% end %}
 
   def buffer : CodeBuffer
-    super.as?(CodeBuffer).not_nil!
+    super.as(CodeBuffer)
   end
 
   def cursor_changed_signal
@@ -131,7 +120,15 @@ class CodeEditor < GtkSource::View
 
   def goto_line(line : Int32, col : Int32)
     iter = buffer.iter_at_line_offset(line, col)
+    goto(iter)
+  end
+
+  def goto(iter)
     buffer.place_cursor(iter)
+    scroll_to_iter(iter)
+  end
+
+  def scroll_to_iter(iter)
     scroll_to_iter(iter, 0.1, true, 0.0, 0.5)
   end
 
@@ -170,62 +167,25 @@ class CodeEditor < GtkSource::View
     buffer.text(start_iter, end_iter, false)
   end
 
-  def search_started : String
+  def selection_or_word_at_cursor : String
     text = if buffer.has_selection?
              buffer.text(*buffer.selection_bounds, false)
            else
              word_at_cursor
            end
-    search_changed(text)
     text
   end
 
-  def search_changed(text : String) : Nil
-    text = GtkSource.utils_unescape_search_text(text)
-    @search_settings.search_text = text
-    @search_mark = buffer.insert
-  end
+  def setup_editor_preferences(path)
+    config = Config.instance
+    resource = self.resource
+    is_make_file = resource.try(&.basename) == "Makefile"
 
-  def search_next : Nil
-    search_iter = buffer.iter_at_mark(@search_mark)
-    search_iter.forward_char
-    valid, start_iter, end_iter = @search_context.forward(search_iter)
-    search_found(start_iter, end_iter) if valid
-    update_label_occurrences_async
-  end
-
-  def search_previous : Nil
-    search_iter = buffer.iter_at_mark(@search_mark)
-    valid, start_iter, end_iter = @search_context.backward(search_iter)
-    search_found(start_iter, end_iter) if valid
-    update_label_occurrences_async
-  end
-
-  private def search_found(start_iter, end_iter)
-    buffer.move_mark(@search_mark, end_iter)
-    buffer.select_range(start_iter, end_iter)
-    scroll_to_iter(end_iter, 0.2, false, 0.0, 0.5)
-  end
-
-  def update_label_occurrences_async
-    GLib.idle_add do
-      update_label_occurrences
-      false
-    end
-  end
-
-  private def update_label_occurrences
-    occurrences_count = @search_context.occurrences_count
-    select_start, select_end = buffer.selection_bounds
-    occurrence_pos = @search_context.occurrence_position(select_start, select_end)
-    text = if occurrences_count == -1
-             ""
-           elsif occurrence_pos == -1
-             "#{occurrences_count} occurrences"
-           else
-             "#{occurrence_pos} of #{occurrences_count}"
-           end
-    self.search_occurences = text
+    @editor.tab_width = is_make_file ? 4 : config.editor_tab_width
+    @editor.insert_spaces_instead_of_tabs = is_make_file ? false : config.editor_insert_spaces_instead_of_tabs
+    @editor.show_right_margin = config.editor_show_right_margin
+    @editor.right_margin_position = config.editor_right_margin_position
+    @editor.highlight_current_line = config.editor_highlight_current_line
   end
 
   def color_scheme=(scheme : Adw::ColorScheme)

commit da2b90c5379b7ec75a489b44f0cbe1c14ed70287

Fix disabled copy path actions.
Regression from 9df6c08c344a164b543b1d93ecb208d0fd5c7a57

src/document_view.cr
@@ -28,6 +28,7 @@ abstract class DocumentView < View
       menu_model: copy_path_menu_model)
     copy_path_btn.add_css_class("flat")
     header_center_box.prepend(copy_path_btn)
+    setup_document_view_actions
   end
 
   def save : Nil
@@ -99,7 +100,7 @@ abstract class DocumentView < View
     "Untitled #{@@untitled_count}"
   end
 
-  private def setup_actions
+  private def setup_document_view_actions
     has_resource = !@resource.nil?
 
     action_group = Gio::SimpleActionGroup.new